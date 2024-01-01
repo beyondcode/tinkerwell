51cd8f992184b23601bc91dc4b873c534a5b9ff5
Jaro Varga <154831101+jarovarga@users.noreply.github.com>
2024-01-01 19:53:30 +0100

    Create CakePhpTinkerwellDriver.php

---
+++ src/Drivers/CakePhpTinkerwellDriver.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Application;
+
+class CakePhpTinkerwellDriver extends TinkerwellDriver
+{
+    public function canBootstrap($projectPath)
+    {
+        return file_exists($projectPath . '/config/bootstrap.php')
+            && file_exists($projectPath . '/vendor/autoload.php')
+            && file_exists($projectPath . '/src/Application.php');
+    }
+
+    public function bootstrap($projectPath)
+    {
+        require $projectPath . '/vendor/autoload.php';
+
+        $application = new Application($projectPath . '/config');
+
+        $application->bootstrap();
+    }
+}
