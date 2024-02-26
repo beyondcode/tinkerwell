913e6c029f3f8fceaf2f267d22af9e75eb5fbd25
cgoedel <christoph.goedel-pavlik@helloly.com>
2024-02-26 16:45:18 +0100

    Change formatting to style requirements

--- src/Drivers/WhmcsTinkerwellDriver.php
+++ src/Drivers/WhmcsTinkerwellDriver.php
@@ -6,23 +6,23 @@ class WhmcsTinkerwellDriver extends TinkerwellDriver
 {
     public function canBootstrap($projectPath)
     {
-        return is_dir($projectPath . '/vendor/whmcs/whmcs-foundation');
+        return is_dir($projectPath.'/vendor/whmcs/whmcs-foundation');
     }
 
     public function bootstrap($projectPath)
     {
-        require_once __DIR__ . '/../vendor/autoload.php';
+        require_once __DIR__.'/../vendor/autoload.php';
 
-        if (!defined("ROOTDIR")) {
-            define("ROOTDIR", realpath(__DIR__ . '/..'));
+        if (! defined("ROOTDIR")) {
+            define("ROOTDIR", realpath(__DIR__.'/..'));
         }
 
-        if (!defined("WHMCS")) {
+        if (! defined("WHMCS")) {
             define("WHMCS", true);
         }
 
-        require_once ROOTDIR . "/includes/dbfunctions.php";
-        require_once ROOTDIR . "/includes/functions.php";
+        require_once ROOTDIR."/includes/dbfunctions.php";
+        require_once ROOTDIR."/includes/functions.php";
 
         $errMgmt = WHMCS\Utility\ErrorManagement::boot();
         $runtimeStorage = new WHMCS\Config\RuntimeStorage();
