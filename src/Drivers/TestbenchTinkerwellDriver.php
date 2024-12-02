5485b6351ec4a53ee3077f313604eadd0c28df6c
Marcel Pociot <m.pociot@gmail.com>
2024-12-02 11:00:23 +0100

    Apply fixes from StyleCI (#23)

--- src/Drivers/TestbenchTinkerwellDriver.php
+++ src/Drivers/TestbenchTinkerwellDriver.php
@@ -4,13 +4,13 @@ class TestbenchTinkerwellDriver extends LaravelTinkerwellDriver
 {
     public function canBootstrap($projectPath)
     {
-        return !file_exists($projectPath . '/public/index.php') &&
-        file_exists($projectPath . '/vendor/orchestra/testbench-core/laravel/public/index.php');
+        return ! file_exists($projectPath.'/public/index.php') &&
+        file_exists($projectPath.'/vendor/orchestra/testbench-core/laravel/public/index.php');
     }
 
     public function bootstrap($projectPath)
     {
-        require_once $projectPath . '/vendor/autoload.php';
+        require_once $projectPath.'/vendor/autoload.php';
 
         $config = \Orchestra\Testbench\Foundation\Config::loadFromYaml(
             workingPath: $projectPath,
@@ -30,6 +30,6 @@ class TestbenchTinkerwellDriver extends LaravelTinkerwellDriver
      */
     public function appVersion()
     {
-        return 'Testbench ' . app()->version();
+        return 'Testbench '.app()->version();
     }
 }
