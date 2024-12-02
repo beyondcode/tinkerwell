2238658763ba9f5a754b875e246e02dd1a9268ea
Marcel Pociot <m.pociot@gmail.com>
2024-12-02 10:59:48 +0100

    Add Testbench driver

---
+++ src/Drivers/TestbenchTinkerwellDriver.php
@@ -0,0 +1,35 @@
+<?php
+
+class TestbenchTinkerwellDriver extends LaravelTinkerwellDriver
+{
+    public function canBootstrap($projectPath)
+    {
+        return !file_exists($projectPath . '/public/index.php') &&
+        file_exists($projectPath . '/vendor/orchestra/testbench-core/laravel/public/index.php');
+    }
+
+    public function bootstrap($projectPath)
+    {
+        require_once $projectPath . '/vendor/autoload.php';
+
+        $config = \Orchestra\Testbench\Foundation\Config::loadFromYaml(
+            workingPath: $projectPath,
+            defaults: [
+                'providers' => [],
+                'dont-discover' => [],
+            ],
+        );
+        $commander = new \Orchestra\Testbench\Console\Commander($config, $projectPath);
+        $commander->laravel();
+    }
+
+    /**
+     * Returns the application version.
+     *
+     * @return string
+     */
+    public function appVersion()
+    {
+        return 'Testbench ' . app()->version();
+    }
+}
