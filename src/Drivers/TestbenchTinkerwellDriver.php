b271c4149da6dcfcc89e575022ac7789c380b7d6
Diana Scharf <scharf.diana@googlemail.com>
2025-06-18 12:28:55 +0200

    Updated TestbenchTinkerwellDriver

--- src/Drivers/TestbenchTinkerwellDriver.php
+++ src/Drivers/TestbenchTinkerwellDriver.php
@@ -32,4 +32,53 @@ class TestbenchTinkerwellDriver extends LaravelTinkerwellDriver
     {
         return 'Testbench '.app()->version();
     }
+
+
+    /**
+     * Returns the PHP files in the application to be referenced in the
+     * Tinkerwell AI Chat.
+     *
+     * @return array
+     */
+    public function appFiles()
+    {
+        $skip = $this->excludeAppFolders;
+
+        $appPath = $this->getBasePath();
+
+        $appPath = str($appPath)->before('vendor')->replaceLast('/', '');
+
+        $files = [];
+        $iterator = new \RecursiveIteratorIterator(
+            new \RecursiveDirectoryIterator($appPath, \FilesystemIterator::SKIP_DOTS)
+        );
+
+
+        foreach ($iterator as $file) {
+            if ($file->getExtension() !== 'php') {
+                continue;
+            }
+
+            $relativePath = substr($file->getPathname(), strlen($appPath) + 1);
+
+            $skipFile = false;
+            foreach ($skip as $folder) {
+                if (strpos($relativePath, $folder . DIRECTORY_SEPARATOR) === 0) {
+                    $skipFile = true;
+                    break;
+                }
+            }
+            if ($skipFile) {
+                continue;
+            }
+
+            $files[] = [
+                'name' => $file->getFilename(),
+                'relativePath' => $relativePath,
+                'fullPath' => $file->getPathname(),
+            ];
+        }
+
+        return $files;
+    }
 }
